d3edf7066b48820fb66e10b80691217d435221dc
ckc <chitkhonncho9@gmail.com>
2023-08-08 20:35:49 +0630

    project uploade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { Routes, RouterModule } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component';
+import { CreateBookComponent } from './create-book/create-book.component';
+import { UpdateBookComponent } from './update-book/update-book.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+const routes: Routes = [
+  { path: 'books', component: BookListComponent },
+  { path: 'create-book', component: CreateBookComponent },
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: 'update-book/:bookCode', component: UpdateBookComponent },
+  { path: 'book-details/:bookCode', component: BookDetailsComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
---
+++ src/app/book.ts
@@ -0,0 +1,6 @@
+export class Book {
+    bookCode!: string
+    bookTitle!: string;
+    bookAuthor!: string;
+    bookPrice!: string; 
+}
